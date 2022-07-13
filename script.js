1ead6fe11bbfe255ce287559a04b39d2c0f05828
Atlas <antonio.claudino@discente.ufma.br>
2022-07-13 00:29:12 -0300

    processo html e css concluído

---
+++ script.js
@@ -0,0 +1,47 @@
+//LISTA DE FIGURE ACTIONS
+let listFigure = [
+    {
+        Imagem: ,
+        Nome: ,
+        Valor: ,
+    }
+    {
+        Imagem: ,
+        Nome: ,
+        Valor: ,
+    }
+    {
+        Imagem: ,
+        Nome: ,
+        Valor: ,
+    }
+]
+
+//LISTA DE PAINTINGS
+let listPainting = [
+    {
+        Imagem: ,
+        Nome: ,
+        Valor: ,
+    }
+    {
+        Imagem: ,
+        Nome: ,
+        Valor: ,
+    }
+    {
+        Imagem: ,
+        Nome: ,
+        Valor: ,
+    }
+]
+
+//CRIAR ITENS DE AÇÃO
+function createActionItem(objeto) {
+    
+}
+
+//CRIAR PINTURAS
+function createPaitingItem(objeto) {
+
+}
