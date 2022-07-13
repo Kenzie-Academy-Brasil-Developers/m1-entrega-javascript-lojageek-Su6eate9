e18e23ad0836b7bfef4463ede0e48ae7a6c27b9e
Atlas <antonio.claudino@discente.ufma.br>
2022-07-13 00:42:33 -0300

    atualização

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
 //LISTA DE FIGURE ACTIONS
+let secaoListFigure = document.getElementsByClassName('action-figures')[0]
+let secaoListPainting = document.getElementsByClassName('paintings')[0]
+
 let listFigure = [
     {
-        Imagem: ,
-        Nome: ,
-        Valor: ,
-    }
+        Imagem: "./assets/painting/clock.jpg",
+        Nome: 'Clock',
+        Valor: 'R$50,00',
+    },
     {
-        Imagem: ,
-        Nome: ,
-        Valor: ,
-    }
+        Imagem: "./assets/painting/gamepad.jpg",
+        Nome: 'GamePad',
+        Valor: 'R$50,00',
+    },
     {
-        Imagem: ,
-        Nome: ,
-        Valor: ,
+        Imagem: "./assets/painting/personagem.jpg",
+        Nome: 'C3-PO',
+        Valor: 'R$150,00',
     }
 ]
 
 //LISTA DE PAINTINGS
 let listPainting = [
     {
-        Imagem: ,
-        Nome: ,
-        Valor: ,
-    }
+        Imagem: "./assets/actions/animewoman.jpg",
+        Nome: 'Woman Figure',
+        Valor: 'R$125,00',
+    },
     {
-        Imagem: ,
-        Nome: ,
-        Valor: ,
-    }
+        Imagem: "./assets/actions/dragonballpersonagem.jpg",
+        Nome: 'Goku Super Sayajin',
+        Valor: 'R$309,00',
+    },
     {
-        Imagem: ,
-        Nome: ,
-        Valor: ,
+        Imagem: "./assets/actions/starwarspersonagem.jpg",
+        Nome: 'Baby Yoda',
+        Valor: 'R$276,00',
     }
 ]
 
